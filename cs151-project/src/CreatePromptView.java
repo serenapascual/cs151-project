2a30c8110236908a432074267a86194e6ce2bea1
serenapascual <serena.a.pascual@gmail.com>
2017-08-03 22:34:30 -0700

    Remember selected view type
    Clicking a date in the month panel does not change the selected view
    type (Day, View, Month, Event)

---
+++ cs151-project/src/CreatePromptView.java
@@ -0,0 +1,115 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class CreatePromptView {
+	public CreatePromptView(final EventModel model) {
+		final JFrame frame = new JFrame();
+		final JPanel panel = new JPanel();
+		final Calendar cal = model.getCal();
+		final SimpleDateFormat startTime = new SimpleDateFormat("hh:mmaa");
+		final Calendar endTime = new GregorianCalendar();
+		endTime.setTime(cal.getTime());
+		endTime.add(Calendar.MINUTE, 30);
+
+		startTime.format(cal.getTime());
+		final JLabel title = new JLabel("Title: ");
+		final JTextField titleField = new JTextField("Untitled event",20);
+		final JLabel date = new JLabel("Date (MM/DD/YYYY): ");
+		final JTextField dateField = new JTextField((cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.YEAR), 20);
+		final JLabel start = new JLabel("Start Time:");
+		final JTextField startField = new JTextField(startTime.format(cal.getTime()), 20);
+		final JLabel end = new JLabel("End Time:");
+		final JTextField endField = new JTextField(startTime.format(endTime.getTime()),20);
+		final JButton saveButton = new JButton("Save");
+		final JButton cancelButton = new JButton("Cancel");
+
+		cancelButton.setOpaque(true);
+		cancelButton.setBorderPainted(false);
+		cancelButton.setFocusable(false);
+		cancelButton.setBackground(new Color(236, 151, 31));
+		cancelButton.setForeground(Color.WHITE);
+		cancelButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+
+		saveButton.setOpaque(true);
+		saveButton.setBorderPainted(false);
+		saveButton.setFocusable(false);
+		saveButton.setBackground(new Color(68, 157, 68));
+		saveButton.setForeground(Color.WHITE);
+		saveButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String title = titleField.getText();
+				String date = dateField.getText();
+				String start = startField.getText();
+				String end = endField.getText();
+				
+				String[] dateArr = date.split("/");
+				int year = Integer.parseInt(dateArr[2]);
+				int day = Integer.parseInt(dateArr[1]);
+				int month = Integer.parseInt(dateArr[0]);
+//				System.out.println(day);
+//				System.out.println(month);
+                Event eventNew = new Event(title, year, month, day, start, end);
+                boolean conflict = false;
+                if(eventNew.getEnd().before(eventNew.getStart()) || eventNew.getStart().equals(eventNew.getEnd())) {
+                	JOptionPane.showMessageDialog(frame,
+                            "The end time cannot be before the start time.",
+                            "Time Conflict",
+                            JOptionPane.WARNING_MESSAGE);
+                        conflict = true;
+                }
+                
+                for(Event events: model.getEvents()) {
+                	if(events.compareTo(eventNew) == 0) {
+                		JOptionPane.showMessageDialog(frame,
+                                "Times cannot overlap. Please ensure that start time is different from end time",
+                                "Time Conflict",
+                                JOptionPane.WARNING_MESSAGE);
+                            conflict = true;
+                            break;
+                	}
+                }
+                
+                if(!conflict) {
+                	model.addEvent(eventNew);
+                }
+                conflict = false;
+                frame.dispose();
+			}
+		});
+		
+		titleField.selectAll();
+		panel.setLayout(new GridLayout(5, 2));
+		panel.add(title);
+		panel.add(titleField);	
+		panel.add(date);
+		panel.add(dateField);
+		panel.add(start);
+		panel.add(startField);
+		panel.add(end);
+		panel.add(endField);
+		panel.add(saveButton);
+		panel.add(cancelButton);
+		frame.add(panel);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+	}
+}
---
+++ cs151-project/src/Loader.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This class saves the events to events.txt when program terminates, and also
+ * loads the events from the text file.
+ * 
+ * @author Himanshu Mehta, Serena Pascual and Cherie Sew
+ *
+ */
+public class Loader {
+	private static ArrayList<Event> eventList = new ArrayList<Event>();
+	public static EventModel model = new EventModel();
+	
+	public Loader(final EventModel model) {
+		this.model = model;
+	}
+	public Loader() {
+		
+	}
+
+	/**
+	 * Loads the events from the text file and then add them to the calendar.
+	 */
+	public static void loadEvent() {
+		Scanner s = null;
+		try {
+			s = new Scanner(new File("input.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("This is your first run! The file was not found");
+			e.printStackTrace();
+		}
+		ArrayList<String> list = new ArrayList<String>();
+		while (s.hasNextLine()) {
+			list.add(s.nextLine());
+		}
+		s.close();
+		for (String values : list) {
+			model.populateEvents(values);
+		}
+	}
+
+	/**
+	 * This method saves events to the events text file
+	 * @param event is an event which is saved to the text file
+	 *            
+	 */
+	public static void saveEvent(ArrayList<Event> event) {
+		for (int i = 0; i < event.size(); i++) {
+			eventList.add(event.get(i));
+		}
+		File f = new File("events.txt");
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(f.getAbsoluteFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		for (Event s : eventList) {
+			try {
+				bw.write(s.toString() + System.getProperty("line.separator"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
